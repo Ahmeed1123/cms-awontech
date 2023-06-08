7426d85f8b9d985ca1564e57977f8ad08a963fda
Mr.SToRM <mazenn99@gmail.com>
2023-06-08 20:16:19 +0300

    fix

---
+++ asites/js/main.js
@@ -0,0 +1,66 @@
+const ssvg = document.getElementById('ssvg');
+const navbar = document.getElementById('navbar');
+const minIcon = document.getElementById('minIcon');
+ document.getElementById('icon').onclick = function () {
+this.classList.toggle('change');
+navbar.classList.toggle('active');
+ssvg.classList.toggle('del');
+minIcon.classList.toggle('del-2');
+}
+
+  document.getElementById("ul-dropdown").onclick =  function() {
+    this.classList.toggle('change');
+  }
+  document.getElementById("ul-dropdown-Two").onclick =  function() {
+    this.classList.toggle('change');
+  }
+  document.getElementById("ul-dropdown-Three").onclick =  function() {
+    this.classList.toggle('change');
+  }
+  document.getElementById("ul-dropdown-Four").onclick =  function() {
+    this.classList.toggle('change');
+  }
+  document.getElementById("ul-dropdown-five").onclick =  function() {
+    this.classList.toggle('change');
+  }
+
+
+document.getElementById("colmun-add-one").onclick = function () {
+    this.classList.toggle("active");
+}
+document.getElementById("colmun-add-two").onclick = function () {
+    this.classList.toggle("active");
+}
+document.getElementById("colmun-add-three").onclick = function () {
+    this.classList.toggle("active");
+}
+document.getElementById("colmun-add-four").onclick = function () {
+    this.classList.toggle("active");
+}
+document.getElementById("colmun-add-five").onclick = function () {
+    this.classList.toggle("active");
+}
+document.getElementById("colmun-add-secth").onclick = function () {
+    this.classList.toggle("active");
+}
+
+
+
+document.getElementById("news-calman-one").onclick = function () {
+    this.classList.toggle("active");
+}
+document.getElementById("news-calman-two").onclick = function () {
+    this.classList.toggle("active");
+}
+document.getElementById("news-calman-three").onclick = function () {
+    this.classList.toggle("active");
+}
+document.getElementById("news-calman-four").onclick = function () {
+    this.classList.toggle("active");
+}
+document.getElementById("news-calman-five").onclick = function () {
+    this.classList.toggle("active");
+}
+document.getElementById("news-calman-secth").onclick = function () {
+    this.classList.toggle("active");
+}
